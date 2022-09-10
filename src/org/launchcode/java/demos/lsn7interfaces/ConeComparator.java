d2fe1bafd79c70acc421422673b96c20e7f6de1c
rebaderbs <rderbak@gmail.com>
2022-09-10 17:03:40 -0500

    "exercises complete for chapter 8"

--- src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -6,13 +6,22 @@ public class ConeComparator implements Comparator<Cone> {
 
     @Override
     public int compare(Cone cone1, Cone cone2) {
-        double price = cone1.getCost() - cone2.getCost();
-        if (price > 0)  {
-            return 1;
-        } else if (price == 0)  {
-            return 0;
-        } else {
+        if (cone1.getCost() - cone2.getCost() < 0){
             return -1;
+        } else if (cone1.getCost() - cone2.getCost() > 0) {
+            return 1;
+        } else {
+            return 0;
         }
     }
+//    public int compare(Cone cone1, Cone cone2) {
+//        double price = cone1.getCost() - cone2.getCost();
+//        if (price > 0)  {
+//            return 1;
+//        } else if (price == 0)  {
+//            return 0;
+//        } else {
+//            return -1;
+//        }
+//    }
 }
--- src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
@@ -5,8 +5,6 @@ import java.util.Comparator;
 public class FlavorComparator implements Comparator<Flavor> {
     @Override
     public int compare(Flavor flavor1, Flavor flavor2)  {
-        int num1 = flavor1.getAllergens().size();
-        int num2 = flavor2.getAllergens().size();
-        return num1 - num2;
+        return flavor1.getName().compareTo(flavor2.getName());
     }
 }
